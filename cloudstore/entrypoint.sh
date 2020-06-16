a355915703901a05726c5ac0af94b4aba891a590
Jonas <jonasuj@hotmail.dk>
2020-06-17 00:45:49 +0200

    Inital boilerplate

---
+++ cloudstore/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# If you are getting an error containing "standard_init_linux.go:211" change the line endings of this
+# file from CRLF to LF
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput --clear
+
+exec "$@"
