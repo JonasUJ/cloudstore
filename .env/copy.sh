5d4ae3b800d08d448f386ffd6973c69562e1740b
JonasUJ <jonasuj@hotmail.dk>
2020-06-18 12:14:47 +0200

    HTTP/2, SSL and CSP

---
+++ .env/copy.sh
@@ -0,0 +1,6 @@
+# Copy the example files to the same files without the .example suffix
+# Be careful not to erase existing files
+cp .env.dev.example .env.dev
+cp .env.nginx.example .env.nginx
+cp .env.prod.example .env.prod
+cp .env.prod.db.example .env.prod.db
